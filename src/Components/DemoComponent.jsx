730881a7a44e5bb296965c48a4fd95f96901874e
katherine szelag <katherine.szelag@mail.utoronto.ca>
2021-06-17 14:24:19 -0400

    feat: initial commit with tds packages

---
+++ src/Components/DemoComponent.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Heading from '@tds/core-heading'
+
+export default function DemoComponent() {
+  return (
+    <div>
+      <Heading level="h1">the demo component</Heading>
+    </div>
+  )
+}
