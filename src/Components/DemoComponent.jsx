31af92c47fe96e34608013da7d7fe3513c87daed
katherine szelag <katherine.szelag@mail.utoronto.ca>
2021-06-18 11:36:18 -0400

    fix: imported components to be ready to use

--- src/Components/DemoComponent.jsx
+++ src/Components/DemoComponent.jsx
@@ -1,5 +1,14 @@
 import React from 'react'
 import Heading from '@tds/core-heading'
+// import FlexGrid from '@tds/core-flex-grid'
+// import ButtonLink from '@tds/core-button-link'
+// import Image from '@tds/core-image'
+// import Box from '@tds/core-box'
+// import Paragrapg from '@tds/core-paragraph'
+// import ChevronLink from '@tds/core-chevron-link'
+// import CherryBlossoms from '../Images/CherryBlossom.png'
+
+
 
 export default function DemoComponent() {
   return (
